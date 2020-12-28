f4ed491f7f267a5d8dc7f4104ab154dd0da582e7
kshvetsova <katshvetsova@gmail.com>
2020-12-28 05:05:50 +0200

    Updates

---
+++ precache-manifest.47bdbe75065047af686409545c688a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d7f303573804836640a9ec465d5676",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f95b9336bf062d251a8",
+    "url": "./static/css/main.41173f7e.chunk.css"
+  },
+  {
+    "revision": "30727a50711fabb77891",
+    "url": "./static/js/2.fadef7d8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.fadef7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f95b9336bf062d251a8",
+    "url": "./static/js/main.0d8e5372.chunk.js"
+  },
+  {
+    "revision": "de9b279484e7264e035a",
+    "url": "./static/js/runtime-main.fab1445e.js"
+  }
+]);
